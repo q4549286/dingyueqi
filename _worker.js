c1ac9ad1111f2b305b6fdfb2225e6068ac426b67
q4549286 <150493637+q4549286@users.noreply.github.com>
2024-07-25 04:19:38 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -11,12 +11,7 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://ct.xxxxxxxx.tk', //可参考内容格式 自行搭建。
-	'https://addressesapi.090227.xyz/ct', //可参考内容格式 自行搭建。
-	'https://addressesapi.090227.xyz/CloudFlareYes', 
-	'https://raw.githubusercontent.com/q4549286/dingyueqi/main/addressesapi.txt',
-	'https://proxyip.aliyun.fxxk.dedyn.io',
-	'https://zzzzzz.rr.nu',
+
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
